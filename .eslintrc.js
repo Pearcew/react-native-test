f44be67e5ddf89bea8885b87825ffaa763a9da7b
王世博 <aqr2972@dingtalk.com>
2023-06-16 14:11:21 +0800

    project init

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  rules: {
+    'react-native/no-inline-styles': 0,
+    'space-infix-ops': 0,
+    semi: 0,
+    'spaced-comment': 0,
+    quotes: 0,
+    'no-trailing-spaces': 0,
+    'comma-dangle': 0,
+  },
+  globals: {
+    GLOBAL: true,
+    LOG: true,
+  },
+};
